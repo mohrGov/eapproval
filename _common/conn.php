816ffbb57a7de67b8cc397bf950f04fc20e1e137
nadia83 <nornadia.ahsalhi@gmail.com>
2019-09-10 16:43:32 +0800

    Update conn.php

--- _common/conn.php
+++ _common/conn.php
@@ -1,8 +1,7 @@
 <?php 
 
   //$db = mysql_connect( "localhost", "username", "password") 
-  $db = mysql_connect( "10.21.81.109", "root", "") 
-	    or die( "Connection Failed!" );
+ 	    or die( "Connection Failed!" );
 
   mysql_select_db( "kpm_eapproval", $db ) 
         or die( "Select database failed 1" );
